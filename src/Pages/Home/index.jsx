3f6bd942cd0842c52fe2166d215046cb10c9a3a8
Erick Aguila Gonzalez <32887190+ErickAguila@users.noreply.github.com>
2024-08-26 13:39:37 +0000

    Prueba desde github.dev

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -31,6 +31,7 @@ function Home() {
       <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
         {renderView()}
       </div>
+      <div>Hola</div>
       <ProductDetail />
     </Layout>
   );
